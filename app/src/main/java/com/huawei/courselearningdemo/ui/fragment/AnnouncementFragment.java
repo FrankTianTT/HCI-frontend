477b86dad6a8c814937b90e008b2405f2db6aa37
frank tian <franktian424@qq.com>
2021-11-02 16:46:22 +0800

    feat: working on #1383#, 添加了对应的fragment

---
+++ app/src/main/java/com/huawei/courselearningdemo/ui/fragment/AnnouncementFragment.java
@@ -0,0 +1,6 @@
+package com.huawei.courselearningdemo.ui.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class AnnouncementFragment extends Fragment {
+}
---
+++ app/src/main/java/com/huawei/courselearningdemo/ui/fragment/ExaminationFragment.java
@@ -0,0 +1,6 @@
+package com.huawei.courselearningdemo.ui.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class ExaminationFragment extends Fragment {
+}
